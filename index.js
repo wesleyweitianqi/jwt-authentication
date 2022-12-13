d61e5fcb6caf2171ce303d2095ce4121e584d9aa
wesleyweitianqi <wesleytianqiwei@gmail.com>
2022-12-12 23:14:23 -0500

    get accessToken

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 require('dotenv').config();
+
 const {ACCESS_TOKEN_SECRET, REFRESH_TOKEN} = process.env
 const express = require("express");
 const jwt = require("jsonwebtoken");
 const app = express();
 app.use(express.json());
+
 const posts = [
   {
     username: "wesley",
@@ -15,18 +17,32 @@ const posts = [
   },
 ];
 
-app.get("/posts", (req, res)=> {
-  res.json(posts)
+app.get("/posts", authenticateToken,  (req, res)=> {
+  console.log(req.user)
+  res.json(posts.filter(post => post.username === req.user.name))
 })
 
-app.get("/login", (req, res)=> {
+app.post("/login", (req, res)=> {
   const username = req.body.username;
-  const user = {username : username};
+  const user = {name : username};
   const accessToken = jwt.sign(user, ACCESS_TOKEN_SECRET);
 
   res.json({accessToken:accessToken});
 })
 
+function authenticateToken(req, res, next) {
+  const authHeader = req.headers['Authorization']
+  console.log(authHeader)
+  const token = authHeader && authHeader.split(" ")[1];
+
+  if (token == null) return res.sendStatus(401);
+
+  jwt.verify(token, ACCESS_TOKEN_SECRET, (err, user)=> {
+    if (err) return res.sendStatus(403);
+    req.user = user;
+    next()
+  })
+}
 app.listen(3000, ()=> {
   console.log("Server is listening to port 3000")
 });
